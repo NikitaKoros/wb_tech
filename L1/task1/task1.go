1b7d631ece6601eb19ade4c8d35db10f563f1533
NikitaKoros <nikitakorostin09@gmail.com>
2025-08-26 16:27:13 +0300

    task3

--- L1/task1/task1.go
+++ L1/task1/task1.go
@@ -1,4 +1,4 @@
-package taskL1
+package main
 
 import "fmt"
 
---
+++ L1/task3/task3.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"sync"
+	"time"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: go run task3.go <number_of_workers>")
+		os.Exit(1)
+	}
+	
+	numWorkers, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Println("Error: number of workers must be non-negative")
+		os.Exit(1)
+	}
+	
+	ch := make(chan int)
+	
+	var wg sync.WaitGroup
+	wg.Add(numWorkers)
+	for i := range numWorkers {
+		go worker(i, ch, &wg)
+	}
+	
+	rand.New(rand.NewSource(time.Now().UnixNano()))
+	for start := time.Now(); time.Since(start) < time.Second; {
+		ch <- rand.Intn(1000)
+		time.Sleep(time.Millisecond * 100)
+	}
+	
+	close(ch)
+	wg.Wait()
+}
+
+func worker(id int, channel <-chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for message := range channel {
+		fmt.Printf("Worker %d: %d\n", id, message)
+	}
+}
