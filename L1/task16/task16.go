23079572121119921c1a927f2a099d9c45392889
NikitaKoros <nikitakorostin09@gmail.com>
2025-09-04 16:21:59 +0300

    task16

---
+++ L1/task16/task16.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+const (
+	SLICE_SIZE = 20
+	MAX_NUM = 100
+)
+
+func main() {
+	slc := make([]int, 0, SLICE_SIZE)
+	
+	random := rand.New(rand.NewSource(time.Now().Unix()))
+	for _ = range SLICE_SIZE {
+		slc = append(slc, random.Intn(MAX_NUM))
+	}
+	
+	sortedArr := quickSort(slc)
+	fmt.Println("Unsorted slice:", slc)
+	fmt.Println("Sorted array:", sortedArr)
+}
+
+func quickSort(arr []int) []int {
+	if len(arr) <= 1 {
+		return arr
+	}
+	
+	mid := len(arr) / 2
+	pivot := arr[mid]
+	
+	left := make([]int, 0, mid)
+	right := make([]int, 0, mid)
+	for i, val := range arr {
+		if i == mid {
+			continue
+		}
+		if val <= pivot {
+			left = append(left, val)
+		} else {
+			right = append(right, val)
+		}
+	}
+	
+	sortedLeft := quickSort(left)
+	sortedRight := quickSort(right)
+	
+	combinedArr := make([]int, 0, len(arr))
+	combinedArr = append(combinedArr, sortedLeft...)
+	combinedArr = append(combinedArr, pivot)
+	combinedArr = append(combinedArr, sortedRight...)
+	
+	return combinedArr
+}
